5ac42ecdfab9e2af0d7a1b0f6b1dc1d4551151af
Ivan-Nikolaenko <inikolaenko@leantech.ai>
2021-11-19 18:03:55 +0600

    Add new code in my project

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class tests {
     @BeforeAll
-    static void setMaxSize(){
-        Configuration.startMaximized=true;
+    static void setMaxSize() {
+        Configuration.startMaximized = true;
     }
+
     @Test
     public void studentRegForm() throws InterruptedException {
         open("https://demoqa.com/automation-practice-form");
@@ -23,21 +26,24 @@ public class tests {
         $("[id=gender-radio-1]").doubleClick();
         $("[id=userNumber]").setValue("0123456789");
         $("[id=dateOfBirthInput]").click();
-        $(by("aria-label","Choose Wednesday, November 10th, 2021")).click();
+//        -> далее интересный способ поиска необходимого значения атрибута :)
+        $(by("aria-label", "Choose Wednesday, November 10th, 2021")).click();
+//         После нахождения элемента использую ввод значения и нажатие на Enter
         $("[id=subjectsInput]").setValue("English").pressEnter();
+//        Пришлось использовать хpath, по другому не удавалось выйти на данный элемент
         $(byXpath("//*[@id=\"hobbiesWrapper\"]/div[2]/div[1]/label")).click();
-        $("[id=uploadPicture]").click();
+//        $("[id=uploadPicture]").doubleClick(); -> не требует нажатия, используем конструкцию Селенида для отправки файла ,которую
+//        использую далее ->
+        File file = $("#uploadPicture").uploadFile(new File("C:\\Users\\User\\IdeaProjects\\QaGuruRep\\filetest.txt"));
+        $("[id=currentAddress]").setValue("Omsk ugly and lost city");
+        $(byXpath("//*[@id=\"state\"]/div/div[1]")).click();
+        $(byXpath("//*[@id=\"state\"]/div/div[1]")).setValue("NCR");
 
 
 
 
 
         Thread.sleep(4000);
-
-
-
-
-
 
 
     }
